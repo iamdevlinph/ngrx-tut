89cd2cebc72c7bf92aeab758c6edd91864bdf7c8
iamdevlinph <iamdevlinph@gmail.com>
2019-06-03 19:16:23 +0800

    stuff

--- src/app/create/create.component.ts
+++ src/app/create/create.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { AppState } from './../app.state';
-import { Tutorial } from '../ducks/tutorial/model';
 import * as TutorialActions from '../ducks/tutorial/actions';
 
 @Component({
